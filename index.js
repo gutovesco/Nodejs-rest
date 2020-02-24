20f6c355632c36cab10ba599a0f9e090b37334c2
Augusto Vesco Raveli <augusto.raveli@CTNDELLHFDXQ23.pampa.compasso>
2020-02-24 15:19:28 -0300

    nodemon config

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(3000, () => console.log("Servidor ta rodando"))
+app.listen(3000, () => console.log("Servidor ta rodando na porta 3000"));
+
+app.get('/atendimentos', (req, res) => res.send(
+    'realizando get de atendimentos'));
